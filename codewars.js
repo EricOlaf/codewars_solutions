241915e7a8e9aaadb76af55484e0f74c91430d87
Eric Olaveson <olavesoneric@gmail.com>
2019-10-05 12:59:27 -0500

    done w cw

--- codewars.js
+++ codewars.js
@@ -26,6 +26,46 @@
 ///////////////////
 
 /*MY ANSWER*/
+// const sumOfDifferences = (arr) => {
+//     let total = 0;
+
+//     arr.length < 2 && 0;
+
+//     arr.sort((a, b) => b-a);
+
+//     for(let i = 0; i < arr.length -1; i++){
+//         total += arr[i] - arr[i + 1];
+//     }
+
+//     return total;
+// }
 
 
 /*COOL ANSWER!!!*/
+
+// const sumOfDifferences = arr => 
+//     arr
+//         .sort((a, b) => b - a)
+//         .map((a, i) => a - arr[i + 1] || 0)
+//         .reduce((a, b) => a + b, 0);
+
+// function sumOfDifferences(arr) {
+//     console.log(arr)
+//     return arr.length > 1 ? Math.max(...arr) - Math.min(...arr) : 0;
+// }
+
+// let sumOfDifferences = (arr) => {
+//     return arr.length > 1 ?
+//     Math.max(...arr) - Math.min(...arr) :
+//     0
+// }
+
+
+// console.log(sumOfDifferences([]), `problem []`)
+// //0
+// console.log(sumOfDifferences([3]), `problem [3]`)
+// //0
+// console.log(sumOfDifferences([1, 5, 8]), `problem [1, 5, 8]`)
+// //7
+// console.log(sumOfDifferences([10, 2, 7]), `problem [10, 2 7]`)
+// //8

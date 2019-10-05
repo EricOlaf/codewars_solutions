855e256d646cf3e7af1ff295aaffdc595237e020
Eric Olaveson <olavesoneric@gmail.com>
2019-10-05 11:58:21 -0500

    starting new cw

--- codewars.js
+++ codewars.js
@@ -1,22 +1,31 @@
+// ///////////////////
+// /*NEW PROBLEM*/
+// ///////////////////
+
+// /*MY ANSWER*/
+
+// // let noSpace = (str) => {
+// //     let ans = ""
+// //     for(let i = 0; i < str.length; i++){
+// //         if(str[i] !== " "){
+// //             ans += str[i] 
+// //         }
+// //     }
+// //     return ans
+// // }
+
+// /*COOL ANSWER!!!*/
+
+// const noSpace = (x) => x.split(' ').join('');
+
+// console.log(noSpace("a b 1 2"))
+// console.log("ab12")
+
 ///////////////////
 /*NEW PROBLEM*/
 ///////////////////
 
 /*MY ANSWER*/
 
-// let noSpace = (str) => {
-//     let ans = ""
-//     for(let i = 0; i < str.length; i++){
-//         if(str[i] !== " "){
-//             ans += str[i] 
-//         }
-//     }
-//     return ans
-// }
 
 /*COOL ANSWER!!!*/
-
-const noSpace = (x) => x.split(' ').join('');
-
-console.log(noSpace("a b 1 2"))
-console.log("ab12")

fefc0ddaef9cd33b3c7d58eadc4b2346e5c9c310
Eric Olaveson <olavesoneric@gmail.com>
2019-10-18 12:44:12 -0500

    last cw

--- codewars.js
+++ codewars.js
@@ -618,3 +618,22 @@
 // console.log(sumTriangularNumbers(4))
 // console.log(sumTriangularNumbers(-1))
 
+///////////////////
+/*NEW PROBLEM*/
+///////////////////
+
+// String.prototype.toAlternatingCase = function () {
+//     let newWord = ""
+//     for(let i = 0; i < this.length; i++){
+//         if(this[i]=== this[i].toLowerCase()){
+//             newWord += this[i].toUpperCase()
+//         } else if(this[i]=== this[i].toUpperCase()){
+//             newWord += this[i].toLowerCase()
+//         } else {
+//             newWord += this[i]
+//         }
+//     }
+//     return newWord
+//   }
+
+// console.log('EricO'.toAlternatingCase())

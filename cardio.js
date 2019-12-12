b582642aff3137388d5862583825b4323c1d9c24
Eric Olaveson <olavesoneric@gmail.com>
2019-12-12 11:43:49 -0600

    new cardio.js file to be tracked in remote

---
+++ cardio.js
@@ -0,0 +1,27 @@
+//CHALLENGE 1: REVERSE A STRING
+
+//const revStr = (str) => str.split("").reverse().join("");
+//const revStr = (str) => str.split("").reduce((revWord, letter)=>revWord= letter +revWord, "");
+//console.log(revStr("Hello"))
+
+
+//CHALLENGE 2: VALIDATE A PALINDROME
+
+//const revStr = (str) => {}
+
+
+//CHALLENGE 3: 
+
+//const revStr = (str) => {}
+
+//CHALLENGE 4: 
+
+//const revStr = () => {}
+
+//CHALLENGE 5: 
+
+//const revStr = () => {}
+
+//CHALLENGE 6: 
+
+//const revStr = () => {}

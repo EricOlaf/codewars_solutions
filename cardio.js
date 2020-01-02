35273367f3745d964b602c51b536e2c673f85227
Eric Olaveson <olavesoneric@gmail.com>
2020-01-02 11:10:17 -0600

    new cardio challenge

--- cardio.js
+++ cardio.js
@@ -151,12 +151,26 @@
 // Take an array of arrays and flatten to a single array
 // ex. [[1, 2], [3, 4], [5, 6], [7]] = [1, 2, 3, 4, 5, 6, 7]
 
-const flat = arr => arr.reduce((total, el) => [...total, ...el],[]);
+//const flat = arr => arr.reduce((total, el) => [...total, ...el],[]);
 
-console.log(flat([[1, 2], [3, 4], [5, 6], [7]]));
+// const flat = arr => [].concat(...arr);
+
+// console.log(flat([[1, 2], [3, 4], [5, 6], [7]]));
 
 //CHALLENGE #4 : ANAGRAM
 // Return true if anagram and false if not
 // ex. 'elbow' === 'below'
 // ex. 'Dormitory' === 'dirty room##'
 
+const ana = (a, b) =>{
+    //omitting the plus in the regex gave me just the letters and not the whole word.
+    const srt = e => e.toLowerCase().match(/[a-z]/g).sort().join("");
+    return srt(a) === srt(b);
+};
+
+console.log(ana('Dormitory', 'dirty room##'));
+
+//CHALLENGE #5 : LETTER CHANGES
+// Change every letter of the string to the one that follows it and capitalize the vowels
+// Z should turn to A
+// ex. 'hello there' === 'Ifmmp UIfsf'

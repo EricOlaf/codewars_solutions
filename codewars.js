33c9ed520071b34242592f23a8e95176705da1f2
Eric Olaveson <olavesoneric@gmail.com>
2020-04-03 12:52:20 -0500

    recursion

--- codewars.js
+++ codewars.js
@@ -1418,3 +1418,39 @@
 ///////////////////
 /*NEW PROBLEM*/
 ///////////////////
+
+//HARD PROBLEM!!!
+function balancedParens(n) {
+    let all = [];
+    let iteration = 0;
+    function parens(left, right, str) {
+
+        console.log('iteration ' + (iteration +=1), '   left ' + left, 'right ' + right, 'str ' + str)
+
+        // if no more brackets can be added then add the final balanced string
+        if (left === 0 && right === 0) {
+            all.push(str);
+            console.log('HIIIIITTTT')
+        }
+
+        // if we have a left bracket left we add it
+        if (left > 0) {
+            console.log('Before, Left', '   left ' + left, 'right ' + right, 'str ' + str)
+            parens(left - 1, right + 1, str + "(");
+            console.log("After, Left", '   left ' + left, 'right ' + right, 'str ' + str)
+        }
+        console.log('MIDDLE')
+        // if we have a right bracket left we add it
+        if (right > 0) {
+            console.log('before, Right', '   left ' + left, 'right ' + right, 'str ' + str)
+            parens(left, right - 1, str + ")");
+            console.log("After, Right", '   left ' + left, 'right ' + right, 'str ' + str)
+        }
+        console.log("END!!!")
+
+    };
+    parens(n, 0, "");
+    return all;
+}
+
+console.log(balancedParens(2));

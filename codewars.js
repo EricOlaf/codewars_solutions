c7a98a5454cdaf63f6ef2f71f945f891c9609e19
Eric Olaveson <olavesoneric@gmail.com>
2020-10-30 11:18:06 -0500

    stub

--- codewars.js
+++ codewars.js
@@ -1761,4 +1761,13 @@ console.log(squareSum([0, 3, 4, 5]));
 ///////////////////
 //
 
+const flip = (d, a) => {
+  if (d === "R") {
+    return a.sort((a, b) => a - b);
+  }
+  return a.sort((a, b) => b - a);
+};
+
+console.log(flip("R", [3, 2, 1, 2]));
+
 // ANOTHER ANSWER

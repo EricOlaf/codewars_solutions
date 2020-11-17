23e975a7f9b21849b1e87210353de677969b234a
Eric Olaveson <olavesoneric@gmail.com>
2020-11-17 10:24:57 -0600

    new cw

--- codewars.js
+++ codewars.js
@@ -2029,3 +2029,9 @@
 ///////////////////
 /*NEW PROBLEM*/
 ///////////////////
+//FIZZZZZ
+const fb = () => {
+  for (let i = 0; i < 101; ) {
+    console.log((++i % 3 ? "" : "fizz") + (i % 5 ? "" : "BUZZ") || i);
+  }
+};

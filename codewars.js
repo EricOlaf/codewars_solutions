22cec19410f0aa6fbe4924eab5d6a517292dc66b
Eric Olaveson <olavesoneric@gmail.com>
2020-12-01 12:06:00 -0600

    stub

--- codewars.js
+++ codewars.js
@@ -2192,3 +2192,4 @@ const checkExam = (a1, a2) => {
 console.log(checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]));
 console.log(checkExam(["a", "a", "c", "b"], ["a", "a", "b", ""]));
 console.log(checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]));
+console.log(checkExam(["b", "c", "b", "a"], ["", "a", "a", "c"]));
